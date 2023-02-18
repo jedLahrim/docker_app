a241ed42d66d0a650caaaeeec83f43db6110ca00
jed <jolix123@gmail.com>
2023-02-18 16:11:53 +0000

    commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:14.15.1-alpine
+
+WORKDIR /app
+COPY package*.json ./
+
+RUN npm install
+COPY . .
+
+EXPOSE 3000
+ENV DB_HOST = 'eventapp-database.ccuvdqrdczzr.us-west-2.rds.amazonaws.com'
+ENV DB_PORT = 3306
+ENV DB_USERNAME = 'admin'
+ENV DB_PASSWORD = 'jolix1235'
+ENV DB = 'event_app'
+
+CMD ["npm", "run", "start:prod"]
---
+++ src/app.module.ts
@@ -0,0 +1,58 @@
+import { CacheModule, Module } from "@nestjs/common";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ScheduleModule } from '@nestjs/schedule';
+
+@Module({
+  imports: [
+    // .env File Or Dashboard
+    ConfigModule.forRoot({
+      cache: true,
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    // CacheModule.register({ isGlobal: true, ttl: 50000 }),
+    ScheduleModule.forRoot(),
+    // TwilioModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (cfg: ConfigService) => ({
+    //     accountSid: cfg.get(TWILIO_ACCOUNT_SID),
+    //     authToken: cfg.get(TWILIO_AUTH_TOKEN),
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        //process.env what you have in CMD
+        const isProd =
+          process.env.STAGE == 'prod' || process.env.STAGE == 'dev';
+        // mysql://<username>:<password>@<host>:<port>/<db_name>
+        return {
+          type: 'mysql',
+          host: isProd
+            ? configService.get('DB_HOST')
+            : 'eventapp-database.ccuvdqrdczzr.us-west-2.rds.amazonaws.com',
+          port: isProd ? configService.get('DB_PORT') : 3306,
+          // ssl: process.env.STAGE == 'prod',
+          /*extra: {
+              ssl: process.env.STAGE == 'prod' ? {rejectUnauthorized: false} : null,
+          },*/
+          username: isProd ? configService.get('DB_USERNAME') : 'admin',
+          password: isProd ? configService.get('DB_PASSWORD') : 'jolix1235',
+          database: isProd ? configService.get('DB') : 'event_app',
+
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+    //
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
